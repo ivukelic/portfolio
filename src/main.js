3062141287f63ed09edf750aa7875a06c9356e59
ivukelic <ivavukelic.gm@gmail.com>
2026-03-23 17:19:16 +0100

    load spellbook

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createRenderer, loadResources } from "./core";
-import { computerScene } from "./scenes";
+import { computerScene, spellbookScene } from "./scenes";
 
 import "./style.css";
 
@@ -7,17 +7,22 @@ window.addEventListener("load", () => {
     const renderer = createRenderer("#canvas");
 
     const scenes = [];
+    const sceneClasses = { computer: computerScene, spellbook: spellbookScene };
 
     loadResources(
         {
             meshes: {
                 computer: "models/computer.glb",
+                cup: "models/office-cup.glb",
+                spellbook: "models/spellbook.glb",
+                quill: "models/quill.glb",
             },
         },
         (resources) => {
             document.querySelectorAll(".scene").forEach((element) => {
-                const view = new computerScene(element);
-                if (view) {
+                const sceneClass = sceneClasses[element.id];
+                if (sceneClass) {
+                    const view = new sceneClass(element);
                     scenes.push(view);
                 } else {
                     console.log("error");
--- src/scenes.js
+++ src/scenes.js
@@ -48,4 +48,52 @@ class computerScene {
     }
 }
 
-export { computerScene };
+class spellbookScene {
+    constructor(htmlElement) {
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(50, 1, 0.1, 100);
+        this.camera.position.z = 5;
+        this.camera.lookAt(0, 0, 0);
+        this.element = htmlElement;
+    }
+
+    getRect() {
+        return this.element.getBoundingClientRect();
+    }
+
+    onRender(renderer, time) {
+        const { top, bottom, left, width, height } = this.getRect();
+        if (bottom < 0 || top > window.innerHeight) {
+            return;
+        }
+
+        this.onTick(time);
+
+        const posY = window.innerHeight - bottom;
+
+        renderer.setViewport(left, posY, width, height);
+        renderer.setScissor(left, posY, width, height);
+
+        this.camera.aspect = width / height;
+        this.camera.updateProjectionMatrix();
+
+        renderer.render(this.scene, this.camera);
+    }
+
+    onInit({ meshes }) {
+        this.mesh = meshes.spellbook.clone();
+        this.mesh.scale.setScalar(6);
+        this.mesh.rotation.y = 2;
+
+        this.scene.add(new THREE.AmbientLight(0xdddddd, 3));
+        this.scene.add(this.mesh);
+    }
+
+    onTick(time) {
+        const lerp = THREE.MathUtils.lerp;
+
+        this.mesh.position.z = lerp(this.mesh.position.z, -0.5, 0.025);
+    }
+}
+
+export { computerScene, spellbookScene };
